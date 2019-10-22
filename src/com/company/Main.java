311972c8536f10a62b773df58b0d9a48bb920fd8
yeji <aa960923@naver.com>
2019-10-22 15:27:01 +0900

    191022

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,41 +5,8 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Main {
-    public int solution(int n) {
-        int tmp=n;
-        String nBinary = "";
-        while(tmp!=0){
-            nBinary+=tmp%2;
-            tmp/=2;
-        }
-        System.out.println(nBinary);
 
-        //String nBinary = Integer.toBinaryString(n);
-        int nCount=0;
-        for(int i=0;i<nBinary.length();i++){
-            if('1' == nBinary.charAt(i)) nCount++;
-        }
-
-
-        int j=0;
-        int jCount=0;
-        for (j=n+1;nCount!=jCount;j++){
-           // String jBinary = Integer.toBinaryString(j);
-            String jBinary = "";
-            int tmp2=j;
-            while(tmp2!=0){
-                jBinary+=tmp2%2;
-                tmp2/=2;
-            }
-            System.out.println(jBinary);
-            jCount=0;
-            for(int i=0;i<jBinary.length();i++){
-                if(jBinary.charAt(i)=='1') jCount++;
-            }
-        }
-        System.out.println(j-1);
-        return j-1;
-    }
+   
 
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
@@ -48,7 +15,7 @@ class Main {
         int[] array1 = {20,5,10};
         int[] array2 = {4,10,15};
 
-        int n =78;
+        int n =3;
         int m = 4;
 
         String[] strArr={"hot", "dot", "dog", "lot", "log", "cog"};
