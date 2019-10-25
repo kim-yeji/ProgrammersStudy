40e8a46206a7c000cb869afece565c9e2ae1765e
yeji <aa960923@naver.com>
2019-10-26 00:47:39 +0900

    191025

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,10 @@ package com.company;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 class Main {
-
-
+  
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
         int[][] arr2 = {{1}};
@@ -19,13 +19,13 @@ class Main {
         int m = 4;
 
         String[] strArr={"hot", "dot", "dog", "lot", "log", "cog"};
-        String s = "hit";
+        String s = "(())()";
         String target = "cog";
 
         String[][] name = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
 
 
         Main main = new Main();
-        main.solution(n);
+        main.solution(s);
     }
 }
