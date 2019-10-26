3e6db20af031cd397d7a5c78c60e5c714a6a8cd6
yeji <aa960923@naver.com>
2019-10-26 18:49:49 +0900

    191026

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.util.PriorityQueue;
 import java.util.Stack;
 
 class Main {
-  
+
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
         int[][] arr2 = {{1}};
@@ -19,7 +19,7 @@ class Main {
         int m = 4;
 
         String[] strArr={"hot", "dot", "dog", "lot", "log", "cog"};
-        String s = "(())()";
+        String s = "-1 -2 -3 -4";
         String target = "cog";
 
         String[][] name = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
