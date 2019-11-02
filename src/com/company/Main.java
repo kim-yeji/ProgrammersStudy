a45be7ebb0f3bb5e3802246bf018399e2a4b4e6f
yeji <aa960923@naver.com>
2019-11-03 03:06:34 +0900

    191102

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,9 @@
-
 package com.company;
 
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Stack;
 
 class Main {
-
-
 
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
@@ -19,13 +16,13 @@ class Main {
         int m = 4;
 
         String[] strArr={"hot", "dot", "dog", "lot", "log", "cog"};
-        String s = "-1 -2 -3 -4";
+        String s = "baabaa";
         String target = "cog";
 
         String[][] name = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
 
 
         Main main = new Main();
-        main.solution(array1);
+        main.solution(s);
     }
 }
