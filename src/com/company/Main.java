2bc6a454b82a0509b005ad7d1d04463a209d6fe2
yeji <aa960923@naver.com>
2019-11-03 14:03:30 +0900

    191103

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,13 @@ import java.util.Stack;
 
 class Main {
 
+
+
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
         int[][] arr2 = {{1}};
 
-        int[] array1 = {2,6,8,14};
+        int[] array1 = {1, 2, 7, 6, 4};
         int[] array2 = {4,10,15};
 
         int n =15;
@@ -23,6 +25,6 @@ class Main {
 
 
         Main main = new Main();
-        main.solution(s);
+        main.solution(array1);
     }
 }
