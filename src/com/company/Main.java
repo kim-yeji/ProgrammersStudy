66510ba87a8136296f652662d40f239f8ba63ad6
yeji <aa960923@naver.com>
2019-11-15 20:08:20 +0900

    191115 fri

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 class Main {
-    public int solution(int n) {
-        int answer = 0;
-        if (n<=1) return 1;
-
-        int num1=1;
-        int num2=1;
-        for (int i=3;i<=n;i++){
-            answer=num1+num2;
-            answer%=1234567;
-            num1=num2;
-            num2=answer;
-        }
-        return answer;
-    }
 
     public static void main(String[] args) throws Exception {
         int[][] arr1 = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
         int[][] arr2 = {{1}};
 
         int[] array1 = {70, 50, 80, 50};
-        int[] array2 = {4,10,15};
+        int[] array2 = {2,6,8,14};
 
         int n =15;
         int m = 4;
@@ -37,6 +23,6 @@ class Main {
 
 
         Main main = new Main();
-        main.solution(strArr);
+        main.solution(array2);
     }
 }
